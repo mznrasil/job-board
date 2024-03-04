86a73e8e168bd29a103aa486c0157325642e62f4
mznrasil <mzn.rasil@gmail.com>
2024-03-04 08:29:37 +0545

    feat: add job filter

---
+++ src/components/JobResults.tsx
@@ -0,0 +1,59 @@
+import prisma from "@/lib/prisma";
+import JobListItem from "@/components/JobListItem";
+import { JobFilterValues } from "@/lib/validation";
+import { Prisma } from "@prisma/client";
+
+interface JobResultsProps {
+  filterValues: JobFilterValues;
+}
+
+const JobResults = async ({
+  filterValues: { q, location, remote, type },
+}: JobResultsProps) => {
+  const searchString = q
+    ?.split(" ")
+    .filter((word) => word.length > 0)
+    .join(" & ");
+
+  const searchFilter: Prisma.JobWhereInput = searchString
+    ? {
+        OR: [
+          { title: { search: searchString } },
+          { companyName: { search: searchString } },
+          { type: { search: searchString } },
+          { location: { search: searchString } },
+          { locationType: { search: searchString } },
+        ],
+      }
+    : {};
+
+  const where: Prisma.JobWhereInput = {
+    AND: [
+      searchFilter,
+      type ? { type } : {},
+      location ? { location } : {},
+      remote ? { locationType: "Remote" } : {},
+      { approved: true },
+    ],
+  };
+
+  const jobs = await prisma.job.findMany({
+    where,
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <div className="grow space-y-4">
+      {jobs.map((job) => (
+        <JobListItem key={job.id} job={job} />
+      ))}
+      {jobs.length === 0 && (
+        <p className="m-auto text-center">
+          No jobs found. Try adjusting your job filters
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default JobResults;
